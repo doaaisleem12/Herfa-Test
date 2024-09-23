c9c054f37060fe0dbb348cd656a08a832b4e6c9a
doaaisleem12 <133912277+doaaisleem12@users.noreply.github.com>
2024-09-23 19:55:51 +0300

    Add files via upload

---
+++ Helpers/CommonMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerfaTest.Helpers
+{
+    internal class CommonMethod
+    {
+        public static void NavigateToURL(string url)
+        {
+            ManageDriver.driver.Navigate().GoToUrl(url);
+        }
+    }
+}
---
+++ Helpers/ManageDriver.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerfaTest.Helpers
+{
+
+        public class ManageDriver
+        {
+            public static IWebDriver driver = new ChromeDriver();
+
+            public static void MaximizeDriver()
+            {
+                driver.Manage().Window.Maximize();
+            }
+
+            public static void CloseDriver()
+            {
+                driver.Quit();
+            }
+
+        }
+    }
